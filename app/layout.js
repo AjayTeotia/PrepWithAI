d33bb9da2d73064f7d3547640ce7a610d7acea56
Ajay Teotia <teotiaajay20@gmail.com>
2024-10-24 13:20:33 +0530

    first-commit

--- app/layout.js
+++ app/layout.js
@@ -28,6 +28,15 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          {process.env.NODE_ENV === "production" && (
+            <script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3655256419195366"
+              crossorigin="anonymous"
+            ></script>
+          )}
+        </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
